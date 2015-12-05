c2c21a7f8aa91e570335e277e082a5b437e61b6c
Vinicius Mello <vinicius.mello@ufba.br>
2015-12-05 12:00:53 -0300

    fillAllPositions in masterView

--- MinorThirds/MasterViewController.swift
+++ MinorThirds/MasterViewController.swift
@@ -26,6 +26,9 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         masterViewController = self
+        if allPositions == nil {
+            allPositions = fillAllPositions()
+        }
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
